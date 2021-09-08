cf2302d09fb4ead7b1fcd6562331a7356c334441
ravis <ravissavoy49@gmail.com>
2021-09-08 16:55:49 -0500

    creating array to find largest to smallest

---
+++ Array/Game.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array
+{
+    class Game
+    {
+       
+        public void Run()
+        {
+            int[] arr = {20, 40, 12, 100, 15};
+            PrintArray(arr);
+        }
+
+        void PrintArray(int[] arr)
+        {
+            
+            for (int num = 0; num < arr.Length; num++)
+            {
+                Console.WriteLine(arr[num]);
+            }
+
+            int largest(int arr[], int num)
+            {
+                int max = arr{0};
+
+                for (num = 1; < max; num++)
+
+                return max;
+            }
+
+            int smallest(int arr[], int num)
+            {
+                int lest = arr{0};
+
+
+            }
+        }
+    }
+}
