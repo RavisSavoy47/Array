a0e3a3f2ddd5147be58fcbb6c005cb888712b008
ravis <ravissavoy49@gmail.com>
2021-09-10 10:14:39 -0500

    Finished array smallest and largest

--- Array/Game.cs
+++ Array/Game.cs
@@ -6,36 +6,52 @@ namespace Array
 {
     class Game
     {
-       
+
+    
         public void Run()
         {
             int[] arr = {20, 40, 12, 100, 15};
             PrintArray(arr);
+            PrintSmallestAndLargest(arr);
+
         }
 
         void PrintArray(int[] arr)
         {
-            
-            for (int num = 0; num < arr.Length; num++)
+
+            foreach (int num in arr)
             {
-                Console.WriteLine(arr[num]);
-            }
-
-            int largest(int arr[], int num)
-            {
-                int max = arr{0};
-
-                for (num = 1; < max; num++)
-
-                return max;
-            }
-
-            int smallest(int arr[], int num)
-            {
-                int lest = arr{0};
-
-
+                Console.WriteLine(num);
             }
         }
+
+        void PrintSmallestAndLargest(int[] arr)
+        {
+            int largest = arr[0];
+            int smallest = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > largest)
+                {
+                    largest = arr[i];
+                }
+
+                if (arr[i] < smallest)
+                {
+                    smallest = arr[i];
+                }
+            }
+
+            Console.WriteLine("Largest: " + largest);
+            Console.WriteLine("Smallest: " + smallest);
+
+        }
+
+
+
+
+
+
     }
 }
